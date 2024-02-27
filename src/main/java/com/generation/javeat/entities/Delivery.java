fc53a0945ebc7e7c23278fa935492f972fc64f54
stefanovergari03 <154620860+stefanovergari03@users.noreply.github.com>
2024-02-27 12:19:42 +0100

    v1 (#1)

---
+++ src/main/java/com/generation/javeat/entities/Delivery.java
@@ -0,0 +1,51 @@
+package com.generation.javeat.entities;
+
+import java.time.LocalTime;
+import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@SuperBuilder
+
+public class Delivery 
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private LocalTime expected_arrival;
+    private int distance;
+    private String paymentMethod, notes;
+
+    @JsonIgnore
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @JsonIgnore
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "restaurant_id")
+    private Restaurant restaurant;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "DishToDelivery", fetch = FetchType.EAGER)
+    private Set<DishToDelivery> dishesDeliveries;
+    
+}
