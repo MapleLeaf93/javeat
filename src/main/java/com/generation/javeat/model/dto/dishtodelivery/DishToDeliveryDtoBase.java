24da96f1ea5d7875442224c2989e24cbd3fbc79b
GAETANO <piegaetano51@gmail.com>
2024-02-27 12:33:10 +0100

    v1

---
+++ src/main/java/com/generation/javeat/model/dto/dishtodelivery/DishToDeliveryDtoBase.java
@@ -0,0 +1,16 @@
+package com.generation.javeat.model.dto.dishtodelivery;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
+
+public class DishToDeliveryDtoBase 
+{
+    private int quantity;
+}
