844181c31840b1a8f99f4a1ad819f47e84c67e90
Stefano <stefanovergari2903@gmail.com>
2024-02-28 09:18:32 +0100

    ConverterSte

---
+++ src/main/java/com/generation/javeat/model/dtoservices/DeliveryConverter.java
@@ -0,0 +1,30 @@
+package com.generation.javeat.model.dtoservices;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.generation.javeat.entities.Delivery;
+import com.generation.javeat.model.dto.delivery.DeliveryDtoR;
+import com.generation.javeat.repositories.DeliveryRepository;
+import com.generation.javeat.repositories.UserRepository;
+
+@Service
+public class DeliveryConverter 
+{
+    @Autowired
+    DeliveryRepository dRepo;
+    @Autowired
+    UserRepository uRepo;
+
+    public Delivery DtoRToDelivery (DeliveryDtoR dto)
+    {
+        return Delivery
+                .builder()
+                .id(dto.getId())
+                .expected_arrival(dto.getExpected_arrival())
+                .distance(dto.getDistance())
+                .paymentMethod(dto.getPaymentMethod())
+                .notes(dto.getNotes())
+                .build();
+    }
+}
