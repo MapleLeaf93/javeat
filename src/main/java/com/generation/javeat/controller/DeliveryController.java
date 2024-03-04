589080acc6b424f5f72deb6d864c5971b832a234
Chiara Belfiore <Chiara@example.com>
2024-03-04 18:41:37 +0100

    robine

--- src/main/java/com/generation/javeat/controller/DeliveryController.java
+++ src/main/java/com/generation/javeat/controller/DeliveryController.java
@@ -10,10 +10,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.service.annotation.GetExchange;
 
 import com.generation.javeat.model.dto.delivery.DeliveryDtoRPost;
-import com.generation.javeat.model.dto.delivery.DeliveryDtoWFull;
 import com.generation.javeat.model.dto.delivery.DeliveryDtoWRecap;
 import com.generation.javeat.model.dtoservices.DeliveryConverter;
 import com.generation.javeat.model.entities.Delivery;
